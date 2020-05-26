ece1834309887f2dc9ede41fa359d7744c1116cb
rmautia <raphaelnyangenya@gmail.com>
2020-05-26 04:10:28 +0300

    implement search testcase for categories.

--- gallery/tests.py
+++ gallery/tests.py
@@ -61,3 +61,12 @@ class ImageTestClass(TestCase):
     def test_get_image_by_id(self):
         images= Image.get_image_by_id(self.image.id)
         self.assertTrue(len(images) == 1)
+
+     def test_search_by_category(self):
+        images = Image.search_by_category('fashion')
+        self.assertTrue(len(images)>0)
+
+    def test_filter_by_location(self):
+        images = Image.fil0ter_by_location('1')
+        print(images)
+        self.assertTrue(len(images)>0)
