a94340149ca6c1921c3ac019e4228deb0b477de2
rmautia <raphaelnyangenya@gmail.com>
2020-05-26 03:41:51 +0300

    update.

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,3 +1,14 @@
 from django.conf.urls import url
+from django.conf import settings
 from . import views
+from django.conf.urls.static import static
 
+
+urlpatterns = [
+    url(r'^$',views.index, name = 'index'),
+    url(r'^location/(?P<image_location>\d+)', views.location_filter, name='location_filter'),
+    url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single,name = 'single'),
+    url(r'^search/', views.search_image, name='search_image'),
+ ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- personalgallery/settings.py
+++ personalgallery/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'gallery',
+    'bootstrap4',
     'pyuploadcare.dj',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -81,8 +83,11 @@ WSGI_APPLICATION = 'personalgallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ralph',
+        'USER': 'raphael',
+        'PASSWORD': 'ranaya',
+
     }
 }
 
@@ -111,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -124,3 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
