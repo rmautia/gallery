a78865e8c3163a06a49ce446f7f88beec1b8c981
rmautia <raphaelnyangenya@gmail.com>
2020-05-26 04:05:33 +0300

    updating a category nature.

--- gallery/tests.py
+++ gallery/tests.py
@@ -16,10 +16,16 @@ class CategoryTestClass(TestCase):
         self.cat.save_category()
         category = Category.objects.all()
         self.assertTrue(len(category) > 0)
-        
+
     def test_delete_method(self):
         self.cat.save_category()
         self.cat.delete_category()
         category = Category.objects.all()
         self.assertTrue(len(category) == 0)
+
+    def test_update(self):
+        category = Category.get_category_id(self.cat.id)
+        category.update_category('nature')
+        category = Category.get_category_id(self.cat.id)
+        self.assertTrue(category.name == 'nature')
     
