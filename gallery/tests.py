e35794e4b19b9f166f2fe2afd8ea385e52ee1b7d
rmautia <raphaelnyangenya@gmail.com>
2020-05-26 04:03:12 +0300

    test category save instance.

--- gallery/tests.py
+++ gallery/tests.py
@@ -6,5 +6,15 @@ from .models import Image, Category,Location
 class CategoryTestClass(TestCase):
     # Set up Method
     def setUp(self):
-        self.cat = Category(name="fashion")
+        self.cat = Category(name="food")
         self.cat.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.cat, Category))
+
+    def test_save_method(self):
+        self.cat.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
+    
