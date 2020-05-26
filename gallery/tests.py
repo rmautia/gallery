aa07f62ceaf0bf2e5863ae5263865551833cd512
rmautia <raphaelnyangenya@gmail.com>
2020-05-26 04:01:28 +0300

    initiate category test class.

--- gallery/tests.py
+++ gallery/tests.py
@@ -2,3 +2,9 @@ from django.test import TestCase
 from .models import Image, Category,Location
 
 # Create your tests here.
+
+class CategoryTestClass(TestCase):
+    # Set up Method
+    def setUp(self):
+        self.cat = Category(name="fashion")
+        self.cat.save_category()
