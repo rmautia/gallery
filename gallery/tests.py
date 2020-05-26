a80a15eafa51d873ee460790ec7e6654af4049e7
rmautia <raphaelnyangenya@gmail.com>
2020-05-26 04:27:20 +0300

    update tests.

--- gallery/tests.py
+++ gallery/tests.py
@@ -62,22 +62,16 @@ class ImageTestClass(TestCase):
         images= Image.get_image_by_id(self.image.id)
         self.assertTrue(len(images) == 1)
 
-     def test_search_by_category(self):
+    def test_search_by_category(self):
         images = Image.search_by_category('fashion')
         self.assertTrue(len(images)>0)
 
     def test_filter_by_location(self):
-        images = Image.fil0ter_by_location('1')
+        images = Image.filter_by_location('1')
         print(images)
         self.assertTrue(len(images)>0)
 
-    def test_update_image(self):
-        self.image.save_image()
-        image = Image.update_image( self.image.id, 'test update', 'my test',self.loc, self.cat)
-        upimage = Image.objects.filter(id = self.image.id)
-        print(upimage)
-        self.assertTrue(Image.name == 'test update')
-
+    
 
 class LocationTestCLass(TestCase):
     #Set up Method
